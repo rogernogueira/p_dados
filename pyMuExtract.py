4bfa99f5583d69a4b1c46de8085729ca22bf1c54
Rogerio Nogueira <roger@uft.edu.br>
2024-10-08 12:18:33 -0300

    Extract_PDFs

---
+++ pyMuExtract.py
@@ -0,0 +1,15 @@
+import pymupdf
+import glob
+
+for file in glob.glob("pdfs/*.pdf"):
+    reader = pymupdf.open(file) 
+    text =""
+    for page in reader:
+        text+=page.get_text()
+    # Save the text to a file
+    name_file = file.split("\\")[-1].split(".")[0]
+    with open(f"txts\\{name_file}.txt", "w", encoding='utf-8') as f:
+        f.write(text)
+
+print(f"Numero de paginas: {len(reader.pages)}")
+
