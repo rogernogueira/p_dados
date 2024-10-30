63100882cf20e475fc0fd363621917a9b6145003
Rogério Nogueira <roger@uft.edu.br>
2024-10-30 15:07:02 -0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,12 +1,44 @@
 from ultralytics import YOLO
-import supervision as sv
-import cv2
-import numpy as np
+from PIL import Image
+import pytesseract
+import json
+import re
 
-model = YOLO('D:\yolo_layout\model\yolov10x_best.pt')  # Load model
-results = model('D:\yolo_layout\\00ff6644-9.png')  # Inference
+# Load model
+model =  YOLO('runs/detect/train/weights/best.pt')
+# load image
+img = Image.open('/app/getQuestions/prova_p_1.png')
 
-results[0].show()
+# Extração de texto com configuração personalizada
 
- # Show results
+results = model(img) 
+results[0].show() 
+# get Bounding box, somente questões
+bb_questions = []
+for result in results:
+    for box in result.boxes:
+        if box.cls == 0:
+            category = box.cls
+            bounding_box = box.xyxy.tolist()
+            bb_questions.append(bounding_box[0])
+            print(f"Category: {category}, Bounding Box: {bounding_box}")
+#gerar imagen de cada bounding box
+text_questions = []
+for i, box in enumerate(bb_questions):
+    img_question = img.crop(box)
+    text = pytesseract.image_to_string(img_question, lang='por')
+    text_questions.append(text)
+#dump questions to json
+#limpar texto
+text_questions = [text.replace('\n', ' ') for text in text_questions]
+# remove multiple spaces
+text_questions = [re.sub(' +', ' ', text) for text in text_questions]
+# remove leading and trailing spaces
+text_questions = [text.strip().lower() for text in text_questions]
+
+with open('questions.json', 'w', encoding='latin-1') as f:
+    json.dump(text_questions, f, indent=3, )
+
+#cli
+#yolo train data=yolo.yaml
 
